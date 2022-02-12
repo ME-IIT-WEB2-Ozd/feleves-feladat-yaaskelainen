30028253c53e2bc31f99e6b6dc71a27630013b7d
yaaskelainen <munka1900905@gmail.com>
2022-02-12 21:46:25 +0100

    Header css 1

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,15 +1,18 @@
 import Weather from "./Weather";
 import Menu from "./Menu";
+import "../../CSS/header.css";
 
 import DailyInfo from "./DailyInfo";
 
 function Header(){
 
     return(
-       <div className="header">
-           <Weather/>
-           <Menu/>
-           <DailyInfo/>
+       <div className="header container">
+           <div className="row">
+                <div className="col-md-4 weather"><Weather/></div>
+                <div className="col-md-4"><Menu/></div>
+                <div className="col-md-4"><DailyInfo/></div>
+           </div>
        </div> 
     );
 
